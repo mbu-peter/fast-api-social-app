d3e908f7fa52cf28b1c02a95e77db28fbcbdb289
Peter Mbugua <pmcode6234@gmail.com>
2024-06-11 15:59:01 +0300

    disable create engine

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from .config import settings
-from psycopg2.extras import RealDictCursor
+
 
 SQL_ALCHEMY_DATABASE_URL = settings.DATABASE_URI
 
--- app/main.py
+++ app/main.py
@@ -3,12 +3,12 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 
-from .database import engine
-from . import models
+# from .database import engine
+
 from .routers import post, user, auth, like
 
 
-models.Base.metadata.create_all(bind=engine)
+# models.Base.metadata.create_all(bind=engine)
 
 
 app = FastAPI()
